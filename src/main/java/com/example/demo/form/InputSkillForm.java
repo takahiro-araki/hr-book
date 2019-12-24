cd2113e6872cca3de0c6cae927c72c7c6d683dcc
nabenabe0258 <nabenabe0258@gmail.com>
2019-12-24 11:15:10 +0900

    バリデーション機能の追加

--- src/main/java/com/example/demo/form/InputSkillForm.java
+++ src/main/java/com/example/demo/form/InputSkillForm.java
@@ -2,6 +2,9 @@ package com.example.demo.form;
 
 import java.util.List;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
 import org.springframework.web.multipart.MultipartFile;
 
 /**
@@ -9,8 +12,13 @@ import org.springframework.web.multipart.MultipartFile;
  *
  */
 public class InputSkillForm {
+	@NotBlank(message = "社員番号を入力してください")
+	@Pattern(message = "半角数字4桁で入力してください", regexp = "^[0-9]{4}$")
 	private String empId;
+	@NotBlank(message = "お名前を入れてください")
 	private String humanName;
+	@NotBlank(message = "入社日を入力してください")
+	@Pattern(message = "入力形式に従ってください", regexp = "^[0-9]{4}-[0-9]{2}-[0-9]{2}$")
 	private String joinDate;
 	private MultipartFile iconImage;
 	private List<String> baseSkillIds;
